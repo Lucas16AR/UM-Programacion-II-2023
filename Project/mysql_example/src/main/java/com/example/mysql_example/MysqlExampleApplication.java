986b5aae71a719debd2a3c524d91263544a31aea
Lucas16AR <luqui0008@gmail.com>
2023-05-03 00:40:14 -0300

    db

---
+++ Project/mysql_example/src/main/java/com/example/mysql_example/MysqlExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysql_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlExampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MysqlExampleApplication.class, args);
+  }
+
+}
